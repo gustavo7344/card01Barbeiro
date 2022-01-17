4525df668776047a91b0a2aa870daaf71b8e698c
Gustavo Ferreira <64665868+gustavo7344@users.noreply.github.com>
2022-01-17 16:47:47 -0300

    Add files via upload

---
+++ estilo/app.js
@@ -0,0 +1,7 @@
+function chama(){
+    alert('Este botão vai direciona o cliente para a suas redes sociais')
+}
+
+function aviso(){
+    alert('Este banner vai abrir uma pequena galeria de fotos')
+}
